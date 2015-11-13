c0deef608cc33de6c8e928df328b1f02983ed91d
Victor Hung <vhung@kth.se>
2015-11-13 13:24:01 +0100

    Added initial project

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandController : MonoBehaviour {
+	private float zDistance = 0;
+    // Use this for initialization
+    void Start () {
+	
+    }
+
+    // Update is called once per frame
+    void Update () {
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		Vector3 position = ray.GetPoint (10);
+		zDistance -= Input.GetAxis("Mouse ScrollWheel");
+		Vector3 new_position = new Vector3(position.x, zDistance, position.y);
+		this.transform.position = new_position;
+
+
+//		this.transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (mouse.x, mouse.y, zDistance));
+    }
+}
