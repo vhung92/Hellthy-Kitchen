224eaf13a0dd1bfa45520c6bbf60e2a468c0141f
unknown <eldhen@hotmail.com>
2015-11-20 15:16:13 +0100

    TV working

---
+++ Assets/_Scripts/MovieScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(AudioSource))]
+public class MovieScript: MonoBehaviour
+{
+    public MovieTexture movTexture;
+    //public AudioSource audio2;
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = movTexture.audioClip;
+        aud.Play();
+        movTexture.Play();
+        //audio2.Play();
+    }
+}
