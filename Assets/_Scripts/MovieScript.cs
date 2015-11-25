833a53271f52246dd9ff12aa718beb9800e42cad
unknown <eldhen@hotmail.com>
2015-11-25 10:39:08 +0100

    Mesh,rigidbodies and tv fix

--- Assets/_Scripts/MovieScript.cs
+++ Assets/_Scripts/MovieScript.cs
@@ -3,15 +3,51 @@ using System.Collections;
 [RequireComponent(typeof(AudioSource))]
 public class MovieScript: MonoBehaviour
 {
-    public MovieTexture movTexture;
-    //public AudioSource audio2;
+	public MovieTexture[] movTexture = new MovieTexture[0];
+	public MovieTexture noise;
+    private MovieTexture Currentmov;
+	private bool noiseOn;
+
     void Start()
     {
-        GetComponent<Renderer>().material.mainTexture = movTexture;
+		int rand = Random.Range (0,movTexture.Length);
+		Currentmov = movTexture[rand];
+        GetComponent<Renderer>().material.mainTexture = movTexture[rand];
         AudioSource aud = GetComponent<AudioSource>();
-        aud.clip = movTexture.audioClip;
+        aud.clip = movTexture[rand].audioClip;
         aud.Play();
-        movTexture.Play();
-        //audio2.Play();
+        movTexture[rand].Play();
+		noiseOn = false;
     }
+	void Update(){
+
+		if (!Currentmov.isPlaying) {
+			Currentmov.Stop();
+			if(noiseOn)
+				selecter ();
+			else
+				StartNoise ();
+		}
+	}
+
+	void selecter(){
+		int rand = Random.Range (0,movTexture.Length);
+		Currentmov = movTexture[rand];
+		GetComponent<Renderer>().material.mainTexture = movTexture[rand];
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = movTexture[rand].audioClip;
+		aud.Play();
+		movTexture[rand].Play();
+		noiseOn = false;
+	}
+	void StartNoise(){
+		Currentmov = noise;
+		GetComponent<Renderer>().material.mainTexture = noise;
+		AudioSource aud = GetComponent<AudioSource>();
+		aud.clip = noise.audioClip;
+		aud.Play();
+		noise.Play();
+		noiseOn = true;
+	}
+
 }
