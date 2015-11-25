71aa8ea83ec3e65c421ed5c6cc6bfc6ae18f4165
unknown <eldhen@hotmail.com>
2015-11-25 16:24:42 +0100

    Fire added, stove is interactive

---
+++ Assets/_Scripts/OwenScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OwenScript : MonoBehaviour {
+
+	public ParticleSystem Fire;
+
+	void Start (){
+		Fire.enableEmission = false;
+	}
+
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		StartCoroutine(Rotate(Vector3.forward * 90f, 3f)) ;
+		Fire.enableEmission = true;
+	}
+
+	IEnumerator Rotate(Vector3 byAngles, float inTime)
+	{
+		Quaternion fromAngle = transform.rotation ;
+		Quaternion toAngle = Quaternion.Euler(transform.eulerAngles + byAngles) ;
+		for(float t = 0f ; t < 1f ; t += Time.deltaTime/inTime)
+		{
+			transform.rotation = Quaternion.Lerp(fromAngle, toAngle, t) ;
+			yield return null ;
+		}
+	}
+
+}
