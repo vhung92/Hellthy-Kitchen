517920a6ddfab5e4da32386415c3ee3ea2dadbd1
Victor Hung <vhung@kth.se>
2015-11-29 16:20:00 +0100

    Added water from the faucet, activated by triggers and colliders

---
+++ Assets/_Scripts/WaterTap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterTap : MonoBehaviour {
+	private bool isRunning = false;
+	private ParticleSystem sinkWater;
+	public GameObject sinkWaterObj;
+
+	void Awake(){
+		sinkWater = sinkWaterObj.GetComponent<ParticleSystem> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		sinkWater.Play ();
+	}
+
+	void OnTriggerExit(Collider other){
+		sinkWater.Stop ();
+	}
+
+	void OnCollisionEnter(Collision other){
+		sinkWater.Play ();
+	}
+
+	void OnCollisionExit(Collision other){
+		sinkWater.Stop ();
+	}
+}
